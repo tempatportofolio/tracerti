7eeb026214d70901cdba44770f55277dba15d176
rfnvokasi <mrfn.mails@gmail.com>
2020-10-10 22:07:56 +0700

    ubahpass,profile,login,logout

--- tracerti/user-setting.php
+++ tracerti/user-setting.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>User Profile | Tracer Study</title>
+  <title>Pengaturan User | Tracer Study</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
